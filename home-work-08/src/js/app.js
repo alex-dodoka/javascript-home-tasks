6aa18d61e0c8ff92f0d117487cca99d650d35a0c
alex-dodoka <neksus.92@mail.ru>
2018-02-13 23:25:29 +0200

    Create a function that simulates multiplication results as in a multiplication table. Implement a function that adds this multiplication to an array.

--- home-work-08/src/js/app.js
+++ home-work-08/src/js/app.js
@@ -1,11 +1,16 @@
+
+
 function multiplicationTable(size) {
     let table = [];
+    for (let i = 2; i <= size; i++) {
+        let multiplier = i;
 
-    for (let i = 1; i <= size; i++) {
-        let multiplier  = i;
+        for (let j = 1; j <= 10; j++) {
+            let res = multiplier * j;
+            table.push(res);
+        }
     }
-
     return table;
 }
 
-multiplicationTable(10);
+
