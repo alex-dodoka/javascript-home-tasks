52f4f6547206ebddeb7b6f9ef597be70455c2e7d
alex-dodoka <neksus.92@mail.ru>
2018-02-13 23:15:30 +0200

    Determination of the first multiplier of multiplication.

---
+++ home-work-08/src/js/app.js
@@ -0,0 +1,11 @@
+function multiplicationTable(size) {
+    let table = [];
+
+    for (let i = 1; i <= size; i++) {
+        let multiplier  = i;
+    }
+
+    return table;
+}
+
+multiplicationTable(10);
