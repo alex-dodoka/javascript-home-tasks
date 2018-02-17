8e1c7d2d2ad078dae92b1a4030a44dd61a2d359b
alex-dodoka <neksus.92@mail.ru>
2018-02-18 00:30:18 +0200

    Small fixes. Now the processing function correctly displays the data in HTML.

--- home-work-08/src/js/app.js
+++ home-work-08/src/js/app.js
@@ -4,24 +4,29 @@ multiplicationTableHolder.innerHTML = render(multiplicationTable(10));
 
 function multiplicationTable(size) {
     let table = [];
-    for (let i = 2; i <= size; i++) {
+
+    for (let i = 1; i <= size; i++) {
         let multiplier = i;
         table[i] = [];
         for (let j = 1; j <= 10; j++) {
             let res = multiplier * j;
             table[i].push(res);
+
         }
     }
     return table;
 }
 
-
 function render(array) {
     let rowsQty = array.length;
     let result = [];
     for (let i = 0; i < rowsQty; i++) {
-        let row = ['<tr><td>', array[i].join('</td><td>'), '</td></tr>'].join('');
-        result.push(row);
+        if (i === 0) {
+            array.splice(i, i);
+        } else {
+            let row = ['<tr><td>', array[i], '</td></tr>'].join('');
+            result.push(row);
+        }
     }
     return result.join('');
 }
