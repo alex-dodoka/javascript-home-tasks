2c667eb4e08236b3cb4e02625b86b7f3ef6960ac
alex-dodoka <neksus.92@mail.ru>
2018-02-19 03:06:07 +0200

    Create a primitive function to add items to the Recycle Bin.

---
+++ shopping-cart-for-online-store/src/js/app.js
@@ -0,0 +1,26 @@
+let rowTemplate = "В корзине";
+
+function addToCart() {
+    let strOfItems = Math.floor($('#number-of-goods').val());
+
+    if (typeof strOfItems !== `number`) {
+        printInHtml(`Введите число!`);
+    }
+    if (strOfItems === 1) {
+        printInHtml(`${rowTemplate} ${strOfItems} товар`);
+    } else if (strOfItems >= 2 || strOfItems <= 4) {
+        printInHtml(`${rowTemplate} ${strOfItems} товарa`);
+    } else {
+        printInHtml(`${rowTemplate} ${strOfItems} товаров`);
+    }
+    clearInput();
+}
+
+
+function printInHtml(data) {
+    $('#print-space').html(data);
+}
+
+function clearInput() {
+    $('#number-of-goods').val('');
+}
