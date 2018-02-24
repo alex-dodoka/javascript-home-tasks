c9b7ef5d03819640734e0370ce262dd8cb8a5e94
alex-dodoka <neksus.92@mail.ru>
2018-02-25 00:11:07 +0200

    small fixes

--- home-work-08/src/js/app.js
+++ home-work-08/src/js/app.js
@@ -3,8 +3,9 @@ let matrixHolder = document.getElementById('matrix');
 
 // multiplicationTableHolder.innerHTML = render(multiplicationTable(10));
 // matrixHolder.innerHTML = render(matrix(10));
+matrix(10);
 
-function multiplicationTable(size) {
+/*function multiplicationTable(size) {
     let table = [];
     for (let i = 0; i <= size - 1; i++) {
         table[i] = [];
@@ -13,25 +14,86 @@ function multiplicationTable(size) {
         }
     }
     return table;
-}
+}*/
 
-function matrix(size) {
+/*function matrix(size) {
     let matrix = [];
     for (let i = 0; i < size; i++) {
         matrix[i] = [];
         for (let j = 0; j < size; j++) {
             if (matrix[i] === matrix[j]) {
                 matrix[i].push(1);
-            } else {
+            } else if (i + j === 9) {
+                matrix[i].push(2)
+            }else {
                 matrix[i].push(0);
             }
         }
-
-
     }
+
+    // for (let i = 0; i < 10; i++) {
+    //     matrix[i] = [];
+    //
+    //     for (let j = 0; j < i; j++) {
+    //
+    //         if (j > j / 2) {
+    //             matrix[i].push(2);
+    //         } else {
+    //             matrix[i].push(3);
+    //         }
+    //
+    //     }
+    //
+    //     for (let j = 10; j > i; j--) {
+    //         // matrix[i].push(5);
+    //         if (j > j / 2) {
+    //             matrix[i].push(6);
+    //         } else {
+    //             matrix[i].push(8);
+    //         }
+    //
+    //     }
+    //
+    // }
+
+
     console.log(matrix);
     return matrix;
 
+}*/
+
+
+function matrix(size) {
+    let matrix = [];
+    for (let j = 1; j <= size; j++) {
+
+        let rowMember = [];
+
+        for (let i = 1; i <= size; i++) {
+            if (i > j && i < (size + 1) - j) {
+                rowMember.push(3);
+            }
+            else if (i === j) {
+                rowMember.push(1);
+            }
+            else if (i < j && i > (size + 1) - j) {
+                rowMember.push(5);
+            }
+            else if (i === (size + 1) - j) {
+                rowMember.push(2);
+            }
+            else if (i < j) {
+                rowMember.push(6);
+            }
+            else {
+                rowMember.push(4);
+            }
+
+        }
+        matrix.push(rowMember);
+
+    }
+    return matrix;
 }
 
 
