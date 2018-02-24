43468aba0b6a1ed2027a0e3b492789141cfa6dd9
alex-dodoka <neksus.92@mail.ru>
2018-02-25 00:13:54 +0200

    small fixes

--- shopping-cart-for-online-store/src/js/app.js
+++ shopping-cart-for-online-store/src/js/app.js
@@ -1,25 +1,20 @@
 const rowTemplate = `В корзине `;
 const cases = [`товар`, `товара`, `товаров`];
 
-
 function addToCart() {
     let strOfItems = +($('#number-of-goods').val());
-
     switch (strOfItems) {
         case 1:
             printInHtml(`${rowTemplate} ${strOfItems} ${cases[0]}`);
             break;
-
         case 2:
         case 3:
         case 4:
             printInHtml(`${rowTemplate} ${strOfItems} ${cases[1]}`);
             break;
-
         default:
             printInHtml(`${rowTemplate} ${strOfItems} ${cases[2]}`);
     }
-
     if (strOfItems > 20) {
         strOfItems %= 10;
         let moreThenTwentyItems = +($('#number-of-goods').val());
@@ -28,20 +23,17 @@ function addToCart() {
             case 1:
                 printInHtml(`${rowTemplate} ${moreThenTwentyItems} ${cases[0]}`);
                 break;
-
             case 2:
             case 3:
             case 4:
                 printInHtml(`${rowTemplate} ${moreThenTwentyItems} ${cases[1]}`);
                 break;
-
             default:
                 printInHtml(`${rowTemplate} ${moreThenTwentyItems} ${cases[2]}`);
         }
     }
     clearInput();
 }
-
 
 function printInHtml(data) {
     $('#print-space').html(data);
