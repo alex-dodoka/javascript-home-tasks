23ceb891cbe5c903b604d3206e38b3777c97e7da
alex-dodoka <neksus.92@mail.ru>
2018-02-24 05:02:05 +0200

    Create a function that adds values ​​to the matrix. If the column number and line number are the same, then the value is one.

--- home-work-08/src/js/app.js
+++ home-work-08/src/js/app.js
@@ -1,17 +1,39 @@
 let multiplicationTableHolder = document.getElementById('multiplication-table');
+let matrixHolder = document.getElementById('matrix');
 
-multiplicationTableHolder.innerHTML = render(multiplicationTable(10));
+// multiplicationTableHolder.innerHTML = render(multiplicationTable(10));
+// matrixHolder.innerHTML = render(matrix(10));
 
 function multiplicationTable(size) {
     let table = [];
-    for (let i = 1; i <= size; i++) {
+    for (let i = 0; i <= size - 1; i++) {
         table[i] = [];
-        for (let j = 1; j <= 10; j++) {
+        for (let j = 0; j <= size; j++) {
             table[i].push(i * j);
         }
     }
     return table;
 }
+
+function matrix(size) {
+    let matrix = [];
+    for (let i = 0; i < size; i++) {
+        matrix[i] = [];
+        for (let j = 0; j < size; j++) {
+            if (matrix[i] === matrix[j]) {
+                matrix[i].push(1);
+            } else {
+                matrix[i].push(0);
+            }
+        }
+
+
+    }
+    console.log(matrix);
+    return matrix;
+
+}
+
 
 function render(array) {
     let rowsQty = array.length;
