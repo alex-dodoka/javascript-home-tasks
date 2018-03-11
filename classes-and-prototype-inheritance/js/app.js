80e4bb2b7bc7a88de3faa798dc836883530b74fd
alex-dodoka <neksus.92@mail.ru>
2018-03-11 15:03:42 +0200

    Create common properties for the Transport class.

--- classes-and-prototype-inheritance/js/app.js
+++ classes-and-prototype-inheritance/js/app.js
@@ -1,9 +1,15 @@
 'use strict';
-const
-
-    defaultTypeOfTransport = "This is some transport";
+const defaultTypeOfTransport = "This is some transport",
+    defaultWeight = "Mass of transport not determined";
 
 
 class Transport {
 
+    constructor(type, weight) {
+        this.type = type || defaultTypeOfTransport;
+        this.weight = weight || defaultWeight;
+    }
 }
+
+let car = new Transport("Car", "3T");
+let scooter = new Transport("","3T");
