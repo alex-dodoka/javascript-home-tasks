42134e65575ddf19efd72422c92064329943ea9e
alex-dodoka <neksus.92@mail.ru>
2018-03-11 10:44:08 +0200

    Output of the Pascal triangle and minor corrections.

--- home-work-08/src/js/app.js
+++ home-work-08/src/js/app.js
@@ -1,74 +1,26 @@
 let multiplicationTableHolder = document.getElementById('multiplication-table');
 let matrixHolder = document.getElementById('matrix');
+let pascalTriangleHolder = document.getElementById('pascal-triangle');
 
-// multiplicationTableHolder.innerHTML = render(multiplicationTable(10));
-// matrixHolder.innerHTML = render(matrix(10));
-matrix(10);
+multiplicationTableHolder.innerHTML = render(multiplicationTable(10));
+matrixHolder.innerHTML = render(matrix(10));
+pascalTriangleHolder.innerHTML = render(pascal(10));
 
-/*function multiplicationTable(size) {
+function multiplicationTable(size) {
     let table = [];
-    for (let i = 0; i <= size - 1; i++) {
+    for (let i = 1; i < size; i++) {
         table[i] = [];
-        for (let j = 0; j <= size; j++) {
+        for (let j = 1; j <= size; j++) {
             table[i].push(i * j);
         }
     }
     return table;
-}*/
-
-/*function matrix(size) {
-    let matrix = [];
-    for (let i = 0; i < size; i++) {
-        matrix[i] = [];
-        for (let j = 0; j < size; j++) {
-            if (matrix[i] === matrix[j]) {
-                matrix[i].push(1);
-            } else if (i + j === 9) {
-                matrix[i].push(2)
-            }else {
-                matrix[i].push(0);
-            }
-        }
-    }
-
-    // for (let i = 0; i < 10; i++) {
-    //     matrix[i] = [];
-    //
-    //     for (let j = 0; j < i; j++) {
-    //
-    //         if (j > j / 2) {
-    //             matrix[i].push(2);
-    //         } else {
-    //             matrix[i].push(3);
-    //         }
-    //
-    //     }
-    //
-    //     for (let j = 10; j > i; j--) {
-    //         // matrix[i].push(5);
-    //         if (j > j / 2) {
-    //             matrix[i].push(6);
-    //         } else {
-    //             matrix[i].push(8);
-    //         }
-    //
-    //     }
-    //
-    // }
-
-
-    console.log(matrix);
-    return matrix;
-
-}*/
-
+}
 
 function matrix(size) {
     let matrix = [];
     for (let j = 1; j <= size; j++) {
-
         let rowMember = [];
-
         for (let i = 1; i <= size; i++) {
             if (i > j && i < (size + 1) - j) {
                 rowMember.push(3);
@@ -88,21 +40,35 @@ function matrix(size) {
             else {
                 rowMember.push(4);
             }
-
         }
         matrix.push(rowMember);
-
     }
     return matrix;
 }
 
+function pascal(size) {
+    let triangle = [];
+    for (let i = 0; i < size; i++) {
+        triangle[i] = [];
+        for (let j = 0; j <= i; j++) {
+            if (j === i) {
+                triangle[i].push(1);
+            }
+            else {
+                let pair = (triangle[i - 1][j - 1] ? triangle[i - 1][j - 1] : 0) + (triangle[i - 1][j] ? triangle[i - 1][j] : 0);
+                triangle[i].push(pair);
+            }
+        }
+    }
+    return triangle
+}
 
 function render(array) {
     let rowsQty = array.length;
     let result = [];
     for (let i = 0; i < rowsQty; i++) {
-        if (i === 0) {
-            array.splice(i, i);
+        if (i === rowsQty[i]) {
+             array[i].splice(i,i);
         } else {
             let row = ['<tr><td>', array[i], '</td></tr>'].join('');
             result.push(row);
@@ -110,3 +76,6 @@ function render(array) {
     }
     return result.join('');
 }
+
+
+
