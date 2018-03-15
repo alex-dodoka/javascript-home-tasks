0eadb1d0db487a2c8f3ffe99c23d45b74e2e3531
alex-dodoka <neksus.92@mail.ru>
2018-03-15 22:25:40 +0200

    Created a function for creating transport with random names and prices.

--- classes-and-prototype-inheritance/js/app.js
+++ classes-and-prototype-inheritance/js/app.js
@@ -1,15 +1,30 @@
 'use strict';
 const defaultTypeOfTransport = "This is some transport",
-    defaultWeight = "Mass of transport not determined";
-
+    defaultWeight = "Mass of transport not determined",
+    arrOfModelNames = ["Reno", "Volkswagen", "Mersedes", "Mazda", "Zaporozhets"],
+    arrOfPrice = [2000, 500000, 11500, 66985, 223658,];
 
 class Transport {
 
-    constructor(type, weight) {
-        this.type = type || defaultTypeOfTransport;
-        this.weight = weight || defaultWeight;
+    constructor(model, cost, fuelConsumption) {
+        this.model = model;
+        this.cost = cost;
+        this.fuelConsumption = fuelConsumption;
     }
+
 }
 
-let car = new Transport("Car", "3T");
-let scooter = new Transport("","3T");
+
+function getRandomName(arrOfNames) {
+    let indexName = Math.floor(Math.random() * arrOfNames.length);
+    return arrOfNames[indexName];
+}
+
+function getRandomPrice(arrOfPrice) {
+    let indexPrice = Math.floor(Math.random() * arrOfPrice.length);
+    return arrOfPrice[indexPrice];
+}
+
+let car = new Transport(getRandomName(arrOfModelNames), getRandomPrice(arrOfPrice), 100);
+let car2 = new Transport(getRandomName(arrOfModelNames), getRandomPrice(arrOfPrice), 100);
+let car3 = new Transport(getRandomName(arrOfModelNames), getRandomPrice(arrOfPrice), 100);
