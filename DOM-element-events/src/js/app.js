55733fbdc0c7bff6acc963015007613da610e8ff
alex-dodoka <neksus.92@mail.ru>
2018-03-23 12:54:07 +0200

    Create custom context window. Try to add the new points for this window.

--- DOM-element-events/src/js/app.js
+++ DOM-element-events/src/js/app.js
@@ -6,5 +6,30 @@ document.onmousedown = function (e) {
     div.style.left = `${x}px`;
     div.style.top = `${y}px`;
     let newElement = place.appendChild(div);
-    newElement.className = "dot";
+   // newElement.className = "dot";  // create dot on page.
 };
+
+btn.oncontextmenu = function (e) {
+    e.preventDefault();
+    createContextWindow(e.clientX, e.clientY);
+
+    let menu = document.getElementsByClassName("context");
+    let pointOfMenu = document.createElement("p");
+    pointOfMenu.innerText = "Point 1";
+    pointOfMenu.innerText = "Point 2";
+    pointOfMenu.innerText = "Point 3";
+    menu.appendChild(pointOfMenu);
+
+};
+
+function createContextWindow(coordX, coordY) {
+    let placeOfClick = document.querySelector("body");
+    let newElement = document.createElement("div");
+    newElement.style.left = `${coordX}px`;
+    newElement.style.top = `${coordY}px`;
+    newElement.className = "context";
+    placeOfClick.appendChild(newElement);
+}
+
+
+
